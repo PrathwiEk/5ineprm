359b0f0d759e2bf206a8563d25472705baed6297
shahirdevp <shahirkm@5ine.in>
2019-11-28 11:18:31 +0530

    drag and droup

--- src/views/project/tasks/work.vue
+++ src/views/project/tasks/work.vue
@@ -5,25 +5,102 @@
 
         <div class="db-container">
             <div class="board">
-                <div class="row mb0">
+                <div class="row mb0 pt20">
 
-                    <div class="col s12 m4 l4">
+                    <div class="col s12">
                         <!-- <div class="card board-card">
                             <div class="board-title ">
                                 <span class="red darken-1">To Do (5)</span>
                             </div>
                         </div> -->
 
-                        <div class="simple-page">
-                            <Container @drop="onDrop">            
-                                <Draggable v-for="item in items" :key="item.id">
-                                    <div class="draggable-item">
-                                    {{item.data}}
-                                    </div>
-                                </Draggable>
-                            </Container>
+                        <div class="col s12 m4 l3">
+                            <div class="board-list-box">
+                                <div class="board-list-title">
+                                    <span>TASK LIST</span> <span> (1)</span>
+                                </div>
+                                <div class="board-list-body">
+                                    
+                                </div>
+                            </div>
                         </div>
-                        {{items}}
+
+                        <div class="col s12 m4 l3">
+                            <div class="board-list-box">
+                                <div class="board-list-title">
+                                    <span>TO DO</span> <span> (1)</span>
+                                </div>
+                                <div class="board-list-body">
+                                    <draggable class="dragArea list-group" :list="list1" :clone="clone" :group="{ name: 'people', pull: pullFunction }" @start="start" >
+                                        <div class="list-group-item z-depth-1" v-for="element in list1" :key="element.id"> 
+                                            <p class="list-item-title">{{ element.name }}</p>  
+                                            <p class="list-item-milestone">My mile stone one1</p>
+                                            <p class="list-item-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, eos?</p>
+                                            <div class="list-item-users">
+                                                <div class="list-item-user" v-for="i in 5">
+                                                    <img src="https://nichemodels.co/wp-content/uploads/2019/03/user-dummy-pic.png" alt="" class="responsive-img">
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                    </draggable>
+                                </div>
+                            </div>
+                            
+                        </div>
+
+                        <div class="col s12 m4 l3">
+                            <div class="board-list-box">
+                                <div class="board-list-title">
+                                    <span>IN PROGRESS</span> <span> (1)</span>
+                                </div>
+                                <div class="board-list-body">
+                                   <draggable class="dragArea list-group" :list="list2" group="people">
+                                        <div class="list-group-item z-depth-1" v-for="element in list2" :key="element.id">
+                                            <p class="list-item-title">{{ element.name }}</p>  
+                                            <p class="list-item-milestone">My mile stone one1</p>
+                                            <p class="list-item-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, eos?</p>
+                                            <div class="list-item-users">
+                                                <div class="list-item-user" v-for="i in 5">
+                                                    <img src="https://nichemodels.co/wp-content/uploads/2019/03/user-dummy-pic.png" alt="" class="responsive-img">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </draggable>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col s12 m4 l3">
+                            <div class="board-list-box">
+                                <div class="board-list-title">
+                                    <span>Completed</span> <span> (1)</span>
+                                </div>
+                                <div class="board-list-body">
+                                    <draggable class="dragArea list-group" :list="list3" group="people">
+                                        <div class="list-group-item z-depth-1" v-for="element in list3" :key="element.id">
+                                            <p class="list-item-title">{{ element.name }}</p>  
+                                            <p class="list-item-milestone">My mile stone one1</p>
+                                            <p class="list-item-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, eos?</p>
+                                            <div class="list-item-users">
+                                                <div class="list-item-user" v-for="i in 5">
+                                                    <img src="https://nichemodels.co/wp-content/uploads/2019/03/user-dummy-pic.png" alt="" class="responsive-img">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </draggable>
+                                </div>
+                            </div>
+                        
+                        </div>
+
+
+                        <rawDisplayer class="col s3" :value="list1" title="List 1" />
+
+                        <rawDisplayer class="col s3" :value="list2" title="List 2" />
+
+                        <rawDisplayer class="col s3" :value="list3" title="List 2" />
+                      
                     </div>
 
                 </div>
@@ -36,19 +113,20 @@
 
 import mainnav from '@/components/dashboard/skkin.vue';
 import snav from '@/components/department/secnav.vue';
-
-import { Container, Draggable } from "vue-smooth-dnd";
-import { applyDrag, generateItems } from "@/utils";
-
+import draggable from 'vuedraggable'
+let idGlobal = 8;
 
 export default {
+
+    display: "Clone on Control",
+    instruction: "Press Ctrl to clone element from list 1",
+    order: 4,
+
     components: {
         mainnav,
         snav,
-        Container, 
-        Draggable,
-        applyDrag,
-        generateItems
+        draggable
+        
     },
  
     data() {
@@ -77,16 +155,37 @@ export default {
                 }
             ],
 
-            items:[{ "id": 1, "data": "Nelco Laboratories, Inc." }, { "id": 2, "data": "Nelco Laboratories, Inc." }, { "id": 3, "data": "Taro Pharmaceuticals U.S.A., Inc." }, { "id": 4, "data": "Liddell Laboratories, Inc." }, { "id": 5, "data": "McKesson" }, { "id": 6, "data": "Cardinal Health" }, { "id": 7, "data": "Novo Nordisk" }, { "id": 8, "data": "Lake Erie Medical DBA Quality Care Products LLC" }, { "id": 9, "data": "Cardinal Health" }, { "id": 10, "data": "Rebel Distributors Corp." }]
-                
+            list1: [
+                { name: "Jesus", id: 1 },
+                { name: "Paul", id: 2 },
+                { name: "Peter", id: 3 }
+            ],
+            list2: [
+                { name: "Luc", id: 5 },
+                { name: "Thomas", id: 6 },
+                { name: "John", id: 7 }
+            ],
+
+            list3: [
+                { name: "Luc", id: 8 },
+                { name: "Thomas", id: 9 },
+                { name: "John", id: 10 }
+            ],
+
+            controlOnStart: true   
+
         };
     },
 
     methods: {
-    onDrop(dropResult) {
-      this.items = applyDrag(this.items, dropResult);
-    console.log(dropResult);
-    
+   clone({ name }) {
+      return { name, id: idGlobal++ };
+    },
+    pullFunction() {
+      return this.controlOnStart ? "clone" : true;
+    },
+    start({ originalEvent }) {
+      this.controlOnStart = originalEvent.ctrlKey;
     }
   }
    
@@ -95,3 +194,26 @@ export default {
 
 
 
+<style  scoped>
+.board-list-box .board-list-title {
+    font-size: 15px;
+    font-weight: 600;
+    border-bottom: 2px solid;
+    padding-bottom: 8px;
+    margin-bottom: 12px
+}
+
+.board .board-list-box:nth-child(1) .board-list-title{border-color:#f44336 }
+.board .board-list-box:nth-child(2) .board-list-title{border-color:#7986cb}
+.board .board-list-box:nth-child(3) .board-list-title{border-color:#ffb74d }
+.board .board-list-box:nth-child(4) .board-list-title{border-color:#43a047}
+.board-list-box .board-list-body .list-group-item{cursor: all-scroll;background: #fff; padding:8px; margin-bottom: 10px; border-radius:4px}
+.list-item-title { margin: 0; color: #333; font-size: 15px; font-weight: 600; }
+.list-item-milestone { margin: 5px 0; font-size: 14px; color: #555; }
+.list-item-description { margin: 2px 0; color: #888; font-size: 13px; }
+.list-item-user { display: inline-block; width: 40px; height: 40px; border-radius: 50%; overflow: hidden; }
+.list-item-users { border-top: 1px solid #80808038; margin-top: 13px; padding-top: 5px; }
+
+
+
+</style>
